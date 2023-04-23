e09a6073763fc835d1e26e43429bed7c1e8afbd0
usmanashrafusman <usman.ashraf033@gmail.com>
2023-04-23 21:59:50 +0500

    some tiny structural changes

---
+++ src/interceptors/index.ts
@@ -0,0 +1,2 @@
+export * from "./request.response.interceptor";
+export * from "./user";
--- src/quiz/quiz.controller.ts
+++ src/quiz/quiz.controller.ts
@@ -3,15 +3,15 @@ import { ResponseDto } from 'src/dtos/response.dto';
 import { QuizService } from './quiz.service';
 import { CreateQuiz } from './dtos/request';
 import { IQuiz, IUser } from 'src/schemas';
-import { CurrentUser } from 'src/decorators';
+import { CurrentUser } from 'src/interceptors';
 
 @Controller("quiz")
 export class QuizController {
   constructor(private readonly userService: QuizService) { }
 
   @Post("create")
-  async registerUser(@Body("data") body: CreateQuiz, @CurrentUser() user:IUser): Promise<ResponseDto<IQuiz>> {
-    const quiz: ResponseDto<IQuiz> = await this.userService.createQuiz(body , user);
+  async registerUser(@Body("data") body: CreateQuiz, @CurrentUser() user: IUser): Promise<ResponseDto<IQuiz>> {
+    const quiz: ResponseDto<IQuiz> = await this.userService.createQuiz(body, user);
     return quiz;
   }
 
